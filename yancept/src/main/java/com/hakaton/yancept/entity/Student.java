f878e60649789df91ff8783c5413ab609f8c985e
Yoonton <mak-sim-2005@mail.ru>
2025-02-20 23:29:23 +0400

    321
    uiygvb

--- yancept/src/main/java/com/hakaton/yancept/entity/Student.java
+++ yancept/src/main/java/com/hakaton/yancept/entity/Student.java
@@ -1,4 +1,37 @@
 package com.hakaton.yancept.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "student")
 public class Student {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "firstName", nullable = false)
+    private String firstName;
+
+    @Column(name = "lastName", nullable = false)
+    private String lastName;
+
+    @Column(name = "patronymic", nullable = false)
+    private String patronymic;
+
+    @Column(name = "year", nullable = false)
+    private long yaer;
+
+    @Column(name = "speciality", nullable = false)
+    private String speciality;
+
+    @Column(name = "group")
+    private long group;
 }
