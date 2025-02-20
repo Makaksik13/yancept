c694eda7e3fb89e84b44ae2bc185bee644e94595
Yoonton <mak-sim-2005@mail.ru>
2025-02-20 23:37:39 +0400

    22

--- yancept/src/main/java/com/hakaton/yancept/entity/Student.java
+++ yancept/src/main/java/com/hakaton/yancept/entity/Student.java
@@ -1,37 +1,40 @@
-package com.hakaton.yancept.entity;
+// package com.hakaton.yancept.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Data;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Table;
+// import lombok.Data;
 
-@Data
-@Entity
-@Table(name = "student")
-public class Student {
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+// @Data
+// @Entity
+// @Table(name = "student")
+// public class Student {
+//     @Id
+//     @Column(name = "id")
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private long id;
 
-    @Column(name = "firstName", nullable = false)
-    private String firstName;
+//     @Column(name = "firstName", nullable = false)
+//     private String firstName;
 
-    @Column(name = "lastName", nullable = false)
-    private String lastName;
+//     @Column(name = "lastName", nullable = false)
+//     private String lastName;
 
-    @Column(name = "patronymic", nullable = false)
-    private String patronymic;
+//     @Column(name = "patronymic", nullable = false)
+//     private String patronymic;
 
-    @Column(name = "year", nullable = false)
-    private long yaer;
+//     @Column(name = "year", nullable = false)
+//     private long yaer;
 
-    @Column(name = "speciality", nullable = false)
-    private String speciality;
+//     @Column(name = "speciality", nullable = false)
+//     private String speciality;
 
-    @Column(name = "group")
-    private long group;
-}
+//     @Column(name = "group")
+//     private long group;
+
+//     @Column(name = "selectedTeacher", nullable = true)
+//     private Teacher selectedTeacher;
+// }
---
+++ yancept/src/main/java/com/hakaton/yancept/entity/Teacher.java
@@ -0,0 +1,5 @@
+package com.hakaton.yancept.entity;
+
+public class Teacher {
+    
+}
