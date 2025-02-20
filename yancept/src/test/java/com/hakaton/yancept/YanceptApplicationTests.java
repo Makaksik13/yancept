cbb43a020926bbe0c3cf17dacf73b326ab753c51
Maksim <165577685+Makaksik13@users.noreply.github.com>
2025-02-20 20:00:27 +0400

    Add files via upload

---
+++ yancept/src/test/java/com/hakaton/yancept/YanceptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakaton.yancept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
