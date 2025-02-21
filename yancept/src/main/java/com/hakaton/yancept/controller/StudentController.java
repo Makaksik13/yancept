852143cf8cc649997a226fb4936f552b73a41f6f
Yoonton <mak-sim-2005@mail.ru>
2025-02-21 12:12:19 +0400

    add gitignore

--- yancept/src/main/java/com/hakaton/yancept/controller/StudentController.java
+++ yancept/src/main/java/com/hakaton/yancept/controller/StudentController.java
@@ -1,4 +1,14 @@
 package com.hakaton.yancept.controller;
 
+import com.hakaton.yancept.service.StudentService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+@RequiredArgsConstructor
 public class StudentController {
+    private final StudentService studentService;
 }
--- yancept/src/main/java/com/hakaton/yancept/service/StudentService.java
+++ yancept/src/main/java/com/hakaton/yancept/service/StudentService.java
@@ -1,4 +1,5 @@
 package com.hakaton.yancept.service;
 
 public interface StudentService {
+
 }
